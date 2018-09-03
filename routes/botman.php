651473daa2acbf4cc7883e8d50c2186d3becd3f3
Jorge I. Meza <jimezam@gmail.com>
2018-09-02 22:14:37 -0500

    Added "ver servicios" command

--- routes/botman.php
+++ routes/botman.php
@@ -3,7 +3,19 @@ use App\Http\Controllers\BotManController;
 
 $botman = resolve('botman');
 
+/*
 $botman->hears('Hi', function ($bot) {
     $bot->reply('Hello!');
 });
 $botman->hears('Start conversation', BotManController::class.'@startConversation');
+*/
+
+$botman->hears('Ver servicios', function ($bot) {
+    $bot->reply("Los servicios actuales son:");
+
+    $servicios = \App\Servicio::orderBy('nombre', 'asc')->get();
+    foreach($servicios as $servicio)
+    {
+        $bot->reply($servicio->nombre." (".$servicio->id.")");
+    }
+});
